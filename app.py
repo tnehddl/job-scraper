d1d0cf6e4e41ff94f5dd1fb67e24b0b000d2582a
tnehddl <idh329@naver.com>
2026-06-19 17:31:19 +0900

    Fix Pages site for GitHub Pages

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-headers = {
+HEADERS = {
       'User-Agent':
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
       'Accept':
